d7ad33f28336cd19fb2c1d49618bd0d32f24c0d1
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-21 10:41:05 +0200

    Fix test names

--- yeoldegameshoppe/tests.py
+++ yeoldegameshoppe/tests.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
 
 
-class FailingTestCase(TestCase):
-    def this_will_fail(self):
+class TestStringMethods(TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+    def test_will_fail(self):
         """This test will fail"""
         self.assertEqual(True, False)
