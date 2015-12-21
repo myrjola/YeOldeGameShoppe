d40590e15cb3f52ab68856dd1e8774f31bc577d8
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-21 13:24:42 +0200

    Fix the last pyflakes warning

--- yeoldegameshoppe/wsgi.py
+++ yeoldegameshoppe/wsgi.py
@@ -7,8 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
