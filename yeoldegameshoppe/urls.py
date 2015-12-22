6f4c9c54d7d7777c9f71f2a9736e656689149397
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-22 08:56:26 +0200

    Add fb login url

--- yeoldegameshoppe/urls.py
+++ yeoldegameshoppe/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns(
     # yogsauth
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', yogsauth.views.login, name='login'),
+    url(r'^fb$', yogsauth.views.facebook, name='facebook'),
 
     # admin
     url(r'^admin/', include(admin.site.urls))
--- yogsauth/views.py
+++ yogsauth/views.py
@@ -1,10 +1,20 @@
-import os
-
 from django.shortcuts import render
 from django.conf import settings
 
+from social_auth.views import complete as social_complete
+from social.backends.facebook import FacebookOAuth2
+
 
 def login(request):
+    return render(
+        request,
+        'login.djhtml',
+        context={'facebook_app_id': settings.SOCIAL_AUTH_FACEBOOK_APP_KEY})
+
+
+def facebook(request):
+    complete = social_complete(request, FacebookOAuth2)
+    print(complete)
     return render(
         request,
         'login.djhtml',
