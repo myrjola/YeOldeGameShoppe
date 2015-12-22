103149ce60cc72bda3369ccd36c01764125acfc9
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-22 11:33:55 +0200

    Create Player and Developer models

--- yogsauth/models.py
+++ yogsauth/models.py
@@ -1,1 +1,19 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
+
+
+class Developer(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+
+
+class Player(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    gamertag = models.fields.CharField(max_length=32,
+                                       unique=True)
+
+    def get_name_for_high_score(self):
+        return self.gamertag or self.user.username
--- yogsauth/tests.py
+++ yogsauth/tests.py
@@ -1,1 +1,44 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth import (get_user_model, models)
+
+from .models import (Player, Developer)
+
+
+class PlayerTestCase(TestCase):
+    def setUp(self):
+        user = get_user_model().objects.create(username="user")
+        user2 = get_user_model().objects.create(username="user2")
+        Player.objects.create(user=user)
+        Player.objects.create(gamertag="awesomegamer",
+                              user=user2)
+
+    def test_get_name_for_highscore(self):
+        """We should use gamertag for high scores, but fallback to username."""
+        with_gamertag = Player.objects.get(gamertag="awesomegamer")
+        self.assertEqual(with_gamertag.get_name_for_high_score(),
+                         "awesomegamer")
+
+        without_gamertag = get_user_model().objects.get(username="user").player
+        self.assertEqual(without_gamertag.get_name_for_high_score(), "user")
+
+    def test_cascading_delete(self):
+        """Deleting the User should get rid of the Player"""
+        user = get_user_model().objects.get(username="user2")
+        user.delete()
+        with self.assertRaises(get_user_model().DoesNotExist):
+            get_user_model().objects.get(username="user2")
+        with self.assertRaises(Player.DoesNotExist):
+            Player.objects.get(gamertag="awesomegamer")
+
+
+class DeveloperTestCase(TestCase):
+    def setUp(self):
+        user = get_user_model().objects.create(username="user")
+        Developer.objects.create(user=user)
+
+    def test_cascading_delete(self):
+        """Deleting the Developer shouldn't get rid of the user"""
+        developer = get_user_model().objects.get(username="user").developer
+        developer.delete()
+        user = get_user_model().objects.get(username="user")
+        self.assertFalse(hasattr(user, 'developer'))
