4197eddc8bc7a1bacfe493363f29a1ee37d893f3
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-22 09:55:48 +0200

    Fix social auth app key

--- yogsauth/views.py
+++ yogsauth/views.py
@@ -10,11 +10,11 @@ def login(request):
     return render(
         request,
         'login.djhtml',
-        context={'facebook_app_id': settings.SOCIAL_AUTH_FACEBOOK_APP_KEY})
+        context={'facebook_app_id': settings.SOCIAL_AUTH_FACEBOOK_KEY})
 
 
 def profile(request):
     return render(
         request,
         'profile.djhtml',
-        context={'facebook_app_id': settings.SOCIAL_AUTH_FACEBOOK_APP_KEY})
+        context={'facebook_app_id': settings.SOCIAL_AUTH_FACEBOOKd_KEY})
