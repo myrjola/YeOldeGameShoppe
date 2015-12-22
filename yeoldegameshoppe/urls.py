ecfa29fe23d11775ea372236a2810d9e3ec3c373
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-22 13:56:01 +0200

    Fix logout

--- yeoldegameshoppe/urls.py
+++ yeoldegameshoppe/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns(
     # yogsauth
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^login$', yogsauth.views.auth_login, name='login'),
-    url(r'^logout$', yogsauth.views.auth_login, name='logout'),
+    url(r'^logout$', yogsauth.views.auth_logout, name='logout'),
     url(r'^accounts/profile/$', yogsauth.views.profile, name='profile'),
 
     # admin
