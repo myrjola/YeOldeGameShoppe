667fa3e3e22836e3643fcff4746f6ddb12900a93
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-24 00:25:21 +0200

    Create email validation data on user creation

--- yogsauth/forms.py
+++ yogsauth/forms.py
@@ -3,6 +3,7 @@ import binascii
 import datetime
 
 from django import forms
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -17,25 +18,27 @@ class UserCreationObligatoryEmailForm(UserCreationForm):
         model = User
         fields = ("username", "password1", "password2", "email")
 
-    def save(self, commit=True):
+    def save(self):
         user = super(UserCreationObligatoryEmailForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
 
         # User needs to validate email address before activation
         user.is_active = False
+        user.save()
         email_validation = EmailValidation()
         email_validation.user = user
 
         # Generate random email activation key
-        token_length = email_validation.activation_key.max_length
-        token = binascii.hexlify(os.urandom(token_length))
+        length = EmailValidation._meta.get_field('activation_key').max_length
+        # A byte is converted to two hex digits, therefore the length needs to
+        # be halved, so that the token will fit.
+        token = binascii.hexlify(os.urandom(int(length / 2)))
         email_validation.activation_key = token
 
         # Don't let validation keys be active too long
-        email_validation.key_expires = (datetime.datetime.now() +
+        email_validation.key_expires = (timezone.now() +
                                         datetime.timedelta(days=2))
 
-        if commit:
-            user.save()
+        email_validation.save()
 
         return user
