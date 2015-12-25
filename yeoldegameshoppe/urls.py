18036807c03e74586dd83cdd41748ede088b4a6a
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-25 13:36:18 +0200

    Use builtin login and logout views

--- yeoldegameshoppe/urls.py
+++ yeoldegameshoppe/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout_then_login
+from django.views.generic import RedirectView
+
 
 import yogsauth.views
 
@@ -8,16 +11,17 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     # By default go to login page for now
-    url(r'^$', yogsauth.views.auth_login),
+    url(r'^$', RedirectView.as_view(pattern_name='login')),
 
     # yogsauth
     url('', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^login$', yogsauth.views.auth_login, name='login'),
-    url(r'^logout$', yogsauth.views.auth_logout, name='logout'),
+    url(r'^login$', login, {'template_name': 'login.djhtml'},
+        name='login'),
+    url(r'^logout$', logout_then_login, name='logout'),
     url(r'^profile/$', yogsauth.views.profile, name='profile'),
     url(r'^register$', yogsauth.views.register, name='register'),
-    url(r'^activate/(?P<user_id>\d+)/(?P<activation_key>.+)$', yogsauth.views.activate,
-        name='activate'),
+    url(r'^activate/(?P<user_id>\d+)/(?P<activation_key>.+)$',
+        yogsauth.views.activate, name='activate'),
 
     # admin
     url(r'^admin/', include(admin.site.urls))
--- yogsauth/views.py
+++ yogsauth/views.py
@@ -2,8 +2,6 @@ import logging
 
 from django.shortcuts import (render, get_object_or_404)
 from django.utils import timezone
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
@@ -14,35 +12,6 @@ from .models import EmailValidation
 
 
 logger = logging.getLogger(__name__)
-
-
-def auth_logout(request):
-    """Logout user"""
-    logout(request)
-    return HttpResponseRedirect(reverse("login"))
-
-
-def auth_login(request):
-    """Login user"""
-
-    next = ''
-    for querydict in (request.POST, request.GET):
-        if 'next' in querydict:
-            next = querydict['next']
-    nextpage = next or reverse("profile")
-    print(nextpage)
-    if request.user.is_authenticated():
-        return HttpResponseRedirect(nextpage)
-
-    auth_form = AuthenticationForm(request, data=request.POST or None)
-
-    context = {"auth_form": auth_form}
-
-    if auth_form.is_valid():
-        login(request, auth_form.get_user())
-        return HttpResponseRedirect(nextpage)
-
-    return render(request, 'login.djhtml', context=context)
 
 
 @login_required
