16783af95fa66e4cc0909d70431666687d5e08ff
Martin Yrjölä <martin.yrjola@gmail.com>
2015-12-30 16:56:47 +0200

    Add remaining models based on the diagram

--- yeoldegameshoppe/settings.py
+++ yeoldegameshoppe/settings.py
@@ -32,7 +32,7 @@ TEMPLATE_DEBUG = True
 
 # Application definition
 
-PROJECT_APPS = ('yeoldegameshoppe', 'yogsbase', 'yogsauth')
+PROJECT_APPS = ('yeoldegameshoppe', 'yogsbase', 'yogsauth', 'yogsgame')
 
 INSTALLED_APPS = (
     'django.contrib.admin',
---
+++ yogsgame/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Game(models.Model):
+    """A model for the computer games in the service."""
+    developer = models.ForeignKey('yogsauth.Developer',
+                                  on_delete=models.CASCADE)
+
+    title = models.fields.CharField(max_length=256, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    url = models.URLField()
+
+
+class GameLicense(models.Model):
+    """A model representing a bought license to play a game."""
+    game = models.ForeignKey('Game',
+                             on_delete=models.CASCADE)
+    player = models.ForeignKey('yogsauth.Player',
+                               on_delete=models.CASCADE)
+
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    bought_at = models.DateTimeField(auto_now_add=True)
+
+
+class HighScore(models.Model):
+    game = models.ForeignKey('Game',
+                             on_delete=models.CASCADE)
+    player = models.ForeignKey('yogsauth.Player',
+                               on_delete=models.CASCADE)
+
+    score = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
